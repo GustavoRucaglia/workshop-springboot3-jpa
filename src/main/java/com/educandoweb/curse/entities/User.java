6575585cc10bfd50d094ee5a3575d110874e33c8
GustavoRucaglia <111381127+gustavo-Bozetti@users.noreply.github.com>
2023-08-14 12:38:06 -0300

    implmentando o Order

--- src/main/java/com/educandoweb/curse/entities/User.java
+++ src/main/java/com/educandoweb/curse/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.curse.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,9 +27,14 @@ public class User implements Serializable {
 	private String password;
 	private String phone;
 	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 		
 	}
+	
 
 	public User(Long id, String name, String email, String password, String phone) {
 		this.id = id;
@@ -35,7 +43,11 @@ public class User implements Serializable {
 		this.password = password;
 		this.phone = phone;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	public Long getId() {
 		return id;
 	}
