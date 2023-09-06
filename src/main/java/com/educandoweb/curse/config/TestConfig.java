f15a30a59b23759550badad98cdd072aa08432fc
GustavoRucaglia <111381127+gustavo-Bozetti@users.noreply.github.com>
2023-09-06 12:53:28 -0300

    payment one-to-one associention

--- src/main/java/com/educandoweb/curse/config/TestConfig.java
+++ src/main/java/com/educandoweb/curse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.curse.entities.Category;
 import com.educandoweb.curse.entities.Order;
 import com.educandoweb.curse.entities.OrderItem;
+import com.educandoweb.curse.entities.Payment;
 import com.educandoweb.curse.entities.Product;
 import com.educandoweb.curse.entities.User;
 import com.educandoweb.curse.entities.enums.OrderStatus;
@@ -73,11 +74,17 @@ public class TestConfig implements CommandLineRunner {
 	    
 	    productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	    
+	    
 	    OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 	    OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 	    OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 	    OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 	    
 	    orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+	    
+	    Payment pay1 = new Payment(null,  Instant.parse("2019-06-20T21:53:07Z"), o1);
+	    o1.setPayment(pay1);
+	    
+	    orderRepository.save(o1);
 	}
 }
